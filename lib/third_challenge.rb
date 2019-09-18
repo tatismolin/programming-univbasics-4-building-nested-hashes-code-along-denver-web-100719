cce58f28a109ec5aaf808771449e47b5a593bc88
Tatiana Smolin <tatismolin@gmail.com>
2019-09-18 22:46:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
         },
       :hero => {
         #your key/value pairs here
-        :name = > "Romeo",
+        :name => "Romeo",
         :age => "15"
         :status => "alive"
         },
